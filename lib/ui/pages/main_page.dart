0b1d4ad7d39f4065410340e39c0df6fab9551ed9
Gustavo <gustavo_figueira@outlook.com>
2022-11-28 22:38:54 -0300

    particles added

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_portfolio/ui/pages/widgets/particles_background.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -8,7 +9,22 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello World'),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            const ParticlesBackground(),
+            Text(
+              'Under maintenance',
+              style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                  ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/ui/pages/widgets/particles_background.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:particles_flutter/particles_flutter.dart';
+
+class ParticlesBackground extends StatelessWidget {
+  const ParticlesBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Center(
+      child: Container(
+        key: UniqueKey(),
+        child: Center(
+          child: CircularParticle(
+            awayRadius: 80,
+            numberOfParticles: 25,
+            speedOfParticles: 1,
+            height: screenHeight,
+            width: screenWidth,
+            onTapAnimation: true,
+            particleColor: Colors.white.withAlpha(150),
+            awayAnimationDuration: const Duration(milliseconds: 600),
+            maxParticleSize: 8,
+            isRandSize: true,
+            isRandomColor: true,
+            randColorList: [
+              Colors.red.withAlpha(210),
+              Colors.white.withAlpha(210),
+              Colors.yellow.withAlpha(210),
+              Colors.green.withAlpha(210)
+            ],
+            awayAnimationCurve: Curves.easeInOutBack,
+            enableHover: true,
+            hoverColor: Colors.white,
+            hoverRadius: 90,
+          ),
+        ),
+      ),
+    );
+  }
+}
